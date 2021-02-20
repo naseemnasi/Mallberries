52ec0aa823c21f55337fe6bed50a1753091933e8
naseemnasi <naseemt123@gmail.com>
2021-02-20 08:07:50 +0000

    backup 20-02-2021

--- mallberyy/settings.py
+++ mallberyy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's-@ifa2eb@0=57(7#@@nnvhh+0_r(5t_gb9ofm$l$*%ub5!p66'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = ['/home/Mallberries/mallberriesdir/Mallberries/static']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
